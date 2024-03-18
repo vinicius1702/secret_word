dc8913beb83116feb874e1910f45cd1407194c39
Vinicius Prado Melo <vinimelo1702@outlook.com>
2024-03-18 18:49:40 -0300

    2°

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className='mb-auto text-body-emphasis text_border'><h1>Secret Word</h1></div>
+  )
+}
+
+export default Header
---
+++ src/data/words.jsx
@@ -0,0 +1,15 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Retrovisor", "Cinto", "Farol", "Buzina"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacaxi", "Uva", "Melancia", "Morango", "Kiwi"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Barriga", "Dedo", "Nariz", "Orelha", "Pé"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Memória", "Placa", "Software", "Hardware"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Variável", "Função", "Loop", "Condição", "Algoritmo"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Macarrão", "Pão", "Queijo", "Sopa", "Salada"],
+    animal: ["Cachorro", "Gato", "Elefante", "Leão", "Tigre", "Pássaro", "Peixe", "Cobra", "Girafa", "Coelho"],
+    país: ["Brasil", "Estados Unidos", "França", "Japão", "China", "Índia", "Alemanha", "Rússia", "Canadá", "Austrália"],
+    profissão: ["Médico", "Professor", "Advogado", "Engenheiro", "Programador", "Arquiteto", "Policial", "Bombeiro", "Dentista", "Chef"],
+    esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Vôlei", "Handebol", "Ginástica", "Surfe", "Boxe", "Atletismo"],
+    instrumento: ["Violão", "Piano", "Bateria", "Flauta", "Guitarra", "Violino", "Trompete", "Saxofone", "Harpa", "Acordeão"],
+    transporte: ["Avião", "Bicicleta", "Barco", "Trem", "Ônibus", "Helicóptero", "Caminhão", "Carroça", "Metrô", "Carruagem"],
+    cor: ["Vermelho", "Azul", "Verde", "Amarelo", "Roxo", "Laranja", "Preto", "Branco", "Cinza", "Marrom"]
+};
